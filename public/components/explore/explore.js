0f0442f03cf37cd1c9bbb0a327240a21833ee3d1
Mark <mrmm112903@gmail.com>
2026-07-24 18:03:59 +0800

    idea form fill out

--- public/components/explore/explore.js
+++ public/components/explore/explore.js
@@ -1,6 +1,9 @@
 function initExplore() {
     const difficultyValue = document.getElementById("difficulty-value");
     const sortValue = document.getElementById("sort-value");
+    const ideaForm = document.getElementById("idea-form-container");
+    const ideaOpenBtn = document.getElementById("btn-idea-open");
+    const ideaCloseBtn = document.getElementById("btn-idea-close");
 
     document.querySelectorAll('input[name="diff"]').forEach(radio => {
         radio.addEventListener("change", () => {
@@ -13,4 +16,23 @@ function initExplore() {
             sortValue.textContent = radio.value;
         });
     });
+
+
+    ideaOpenBtn.addEventListener("click", () => {
+        ideaForm.classList.add("show");
+        document.body.classList.add("no-scroll");
+    });
+
+    ideaCloseBtn.addEventListener("click", () => {
+        ideaForm.classList.remove("show");
+        document.body.classList.remove("no-scroll");
+    });
+
+    ideaForm.addEventListener("click", (e) => {
+        if(e.target === ideaForm) {
+            ideaForm.classList.remove("show");
+            document.body.classList.remove("no-scroll");
+        }
+    });
 }
+
