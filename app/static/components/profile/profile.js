df536ed292a6ed920f473a3c5b0b49776aa84b80
Mark <mrmm112903@gmail.com>
2026-07-31 16:38:06 +0800

    adding anim on prof

--- app/static/components/profile/profile.js
+++ app/static/components/profile/profile.js
@@ -1,8 +1,35 @@
 const statButtons = document.querySelectorAll(".profile-stats-row");
 const ideasContainer = document.querySelector(".ideas-container");
 
+let previousScrollPosition = 0;
+
 statButtons.forEach(button => {
     button.addEventListener("click", () => {
-        ideasContainer.classList.toggle("show");
+
+        const isOpening = !ideasContainer.classList.contains("show");
+
+        if (isOpening) {
+
+            previousScrollPosition = window.scrollY;
+
+            ideasContainer.classList.add("show");
+
+            setTimeout(() => {
+                window.scrollBy({
+                    top: 180,
+                    behavior: "smooth"
+                });
+            }, 150);
+
+        } else {
+            ideasContainer.classList.remove("show");
+
+            setTimeout(() => {
+                window.scrollTo({
+                    top: previousScrollPosition,
+                    behavior: "smooth"
+                });
+            }, 150);
+        }
     });
 });
